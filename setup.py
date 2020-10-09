7f6f10c59f64c4e2d3772e3806466f79fa6b57e4
jayathungek <jayathunge.work@gmail.com>
2020-10-09 20:54:45 +0530

    automatically detect packages

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 import setuptools
-from setuptools import find_namespace_packages
+from setuptools import find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="ytam",
-    version="0.0.31",
+    version="0.0.32",
     author="jayathungek",
     author_email="jayathunge.work@gmail.com",
     description="A commandline utility that enables the creation of albums from Youtube playlists.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jayathungek/ytam",
-    packages=["ytam"],
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
