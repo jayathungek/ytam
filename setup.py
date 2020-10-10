aee98b29e7e163cb8db9fd418a21fa6b99e760e7
jayathungek <jayathunge.work@gmail.com>
2020-10-10 12:04:41 +0530

    bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ytam",
-    version="0.0.35",
+    version="0.1.0",
     author="jayathungek",
     author_email="jayathunge.work@gmail.com",
     description="A commandline utility that enables the creation of albums from Youtube playlists.",
