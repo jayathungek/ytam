7ada61768b680423b6b1fc6ca73d412e30295d0f
jayathungek <jayathunge.work@gmail.com>
2022-02-26 19:00:52 +0000

    use discogs api to extract metadata

--- ytam/discogs.py
+++ ytam/discogs.py
@@ -3,24 +3,25 @@ import sys
 import re
 import json
 from urllib.request import Request, urlopen
-from bs4 import BeautifulSoup
 
 try:
     import error
 except ModuleNotFoundError:
     import ytam.error as error
 
+IMAGE_TAG = "og:image"
 ARTIST_TAG = "profile_title"
 ALBUM_TAG = "section_content marketplace_box_buttons_count_1"
-IMAGE_TAG = "image_gallery image_gallery_large"
 TRACKLIST_TAG = "playlist"
 TITLE_TAG = "tracklist_track_title"
 
-discogs_url = r"(https?:\/\/)?(www\.)?discogs\.com\/(release|master)\/.+"
+discogs_url = r"(https?:\/\/)?(www\.)?discogs\.com\/release\/(\d+)-.+"  # release id is stored in 3rd capture group
+discogs_api_url = "https://api.discogs.com/releases"
 artist_exp = r".+ \(\d+\)$"
-
+image_exp = r"(?<=property=\"og:image\" content=\")(.+?)(?=\"(\/)?>)"
 discogs_url_pattern = re.compile(discogs_url)
 artist_pattern = re.compile(artist_exp)
+image_pattern = re.compile(image_exp)
 
 
 def clean_artist(artist):
@@ -30,6 +31,11 @@ def clean_artist(artist):
     if artist_pattern.match(artist):
         return " ".join(artist.split(" ")[:-1])
     return artist
+
+
+def extract_release_id(discogs_release_url):
+    match = discogs_url_pattern.match(discogs_release_url)
+    return match.groups()[2]
 
 
 class Discogs:
@@ -43,6 +49,10 @@ class Discogs:
         if not discogs_url_pattern.match(discogs_release_url):
             raise error.WrongMetadataLinkError(discogs_release_url)
 
+        self.extract_image(discogs_release_url)
+        self.extract_metadata(extract_release_id(discogs_release_url))
+
+    def extract_image(self, discogs_release_url):
         try:
             req = Request(discogs_release_url)
             req.add_header(
@@ -57,41 +67,30 @@ class Discogs:
             raise error.BrokenDiscogsLinkError(discogs_release_url)
 
         html_str = html_bytes.decode("utf8")
-        self.html = BeautifulSoup(html_str, features="html.parser")
-        self.extract_image()
-        self.extract_artist_album()
-        self.extract_tracklist()
-
-    def extract_image(self):
-        image_json = self.html.find("div", class_=IMAGE_TAG)
-        if image_json:
-            image_json = image_json["data-images"]
-            images = json.loads(image_json)
-            highest_res = images[0]["full"]
-            self.image = highest_res
+        image_tag = image_pattern.findall(html_str)
+        if len(image_tag) > 0:
+            self.image = image_tag[0][0]
         else:
             raise error.AlbumArtNotFoundError()
 
-    def extract_artist_album(self):
-        art_alb = self.html.find("h1", id=ARTIST_TAG)
-        if art_alb:
-            self.artist = clean_artist(art_alb.find("a").contents[0])
-            self.album = art_alb.find_all("span")[-1].contents[0].strip()
-        else:
-            raise error.AlbumArtistNotFoundError()
-
-    def extract_tracklist(self):
-        table = self.html.find("table", class_=TRACKLIST_TAG).find_all("tr")
-        if table:
-            titles = []
-            for track in table:
-                title = track.find("span", class_=TITLE_TAG).contents[0]
-                titles.append(title)
-
-            self.tracks = titles
+    def extract_metadata(self, release_id):
+        try:
+            req = Request(f"{discogs_api_url}/{release_id}")
+            req.add_header(
+                "User-Agent",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/50.0.2661.102 Safari/537.36",
+            )
+            fp = urlopen(req)
+            html_bytes = fp.read()
+            metadata = json.loads(html_bytes.decode("utf8"))
+            fp.close()
+            self.artist = clean_artist(metadata["artists"][0]["name"])
+            self.album = metadata["title"]
+            self.tracks = [title["title"] for title in metadata["tracklist"]]
             self.num_tracks = len(self.tracks)
-        else:
-            raise error.AlbumTracklistNotFoundError()
+        except Exception:
+            raise error.BrokenDiscogsLinkError(f"{discogs_api_url}/{release_id}")
 
     def make_file(self, path):
         with open(path, "w") as fh:
@@ -111,3 +110,4 @@ if __name__ == "__main__":
         print(t)
 
     d.make_file("ytam/metadata/title.txt")
+
